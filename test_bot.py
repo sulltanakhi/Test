6c73df6d9c0057a63e29ff1d52c8afbae1604372
sulltanakhi <168452745+sulltanakhi@users.noreply.github.com>
2024-12-17 14:28:08 +0500

    Create test_bot.py
    version 1.0

---
+++ test_bot.py
@@ -0,0 +1,18 @@
+budget=input('Enter your budget')
+workers=input('Enter amount of workers')
+while True:
+    try:
+        budget=int(budget)
+        workers=int(workers)
+        total=budget/workers
+        print(total)
+        break
+    except ZeroDivisionError:
+        print('can not divide by zero')
+        workers=input('Enter amount of workers')
+     except:
+         workers=input('Enter amount of workers as number') 
+         budget=input('Enter your budget as number')   
+
+
+
